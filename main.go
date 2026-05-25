0d911176af6f18e58cbf7354e74ebb12b3b3ec1c
sakusshun <matizzova.a@icloud.com>
2026-05-25 20:09:50 +0300

    Add second exercise: simple greeting message

--- main.go
+++ main.go
@@ -3,10 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fruits := []string{"яблоко", "банан", "апельсин"}
-	fmt.Println(fruits)
-
-	fruits = append(fruits, "киви", "манго")
-	fmt.Println(fruits)
-	fmt.Println(fruits[1])
+	fmt.Println("I am saving my code to GitHub!")
+	fmt.Println("This is my second exercise")
 }
