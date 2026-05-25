02e59897d32cd2d89a9b3374c238893a9496fbba
sakusshun <matizzova.a@icloud.com>
2026-05-25 22:37:04 +0300

    test len(-)

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	friends := []string{"Аня", "Кирилл", "Настя", "Анатолий"}
+	numbers := []int{15, 22, 37, 40, 55, 62}
 
-	for i := 0; i <= 3; i++ {
-		fmt.Println("Привет! ", friends[i])
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i]%2 == 0 {
+			fmt.Println(numbers[i])
+		}
 	}
+
 }
